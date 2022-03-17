7a41777a2a355b1ce3396d00ba167637017e3e37
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-17 20:27:23 +0900

    save username in localstorage

--- index.js
+++ index.js
@@ -14,6 +14,36 @@ function getClock() {
 getClock();
 setInterval(getClock, 1000);
 
+
+// save username to localstorage 
+const userLoginForm = document.getElementById("login-form");
+const usernameInput = document.getElementById("login-form__username");
+const userComponentMe = document.getElementById("user-component__me");
+const CHATUSERNAME_KEY = "chatusername";
+const savedChatUsername = localStorage.getItem(CHATUSERNAME_KEY);
+const currentLink = document.location.href;
+
+function onChatLoginSubmit() {
+    const username = usernameInput.value;
+    localStorage.setItem(CHATUSERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    const userName = userComponentMe.querySelector("h4");
+    userName.innerText = username;
+}
+
+if (savedChatUsername === null) {
+    userLoginForm.addEventListener("submit", onChatLoginSubmit);
+} else {
+    if(currentLink === 'file:///C:/Users/sxxhn/Desktop/Programming/nomad_coders/chat-clone/index.html'){
+        window.location.replace('file:///C:/Users/sxxhn/Desktop/Programming/nomad_coders/chat-clone/friends.html');
+    }
+    paintGreetings(savedChatUsername); 
+}
+
+
 // friends channel-toggle
 
 const channelToggle = document.getElementById("channelToggle");
